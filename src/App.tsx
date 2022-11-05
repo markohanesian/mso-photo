6bc9c7bfc924ba7f441e6c7a9430a8d7b474cdce
mso872 <mso872@gmail.com>
2022-11-04 22:40:15 -0700

    Fix gallery width

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AccountMenu from "../src/components/nav/NavBar";
+import MasonryImageList from "../src/components/image list/ImageList"
 
 function App() {
   return (
     <>
-        <AccountMenu />
+      <AccountMenu />
+      <MasonryImageList />
     </>
   );
 }
