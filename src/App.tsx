a17ffc0c6115e38e9a0fbe7e4628a18a93674e08
mso872 <mso872@gmail.com>
2022-12-27 22:11:23 -0800

    Install router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import AccountMenu from "../src/components/nav/NavBar";
-import home from "../src/pages/home/home"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// pages
+import Home from "./pages/Home/Home"
 
-function App() {
+const App = () => {
   return (
-    <>
-      <AccountMenu />
-    </>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          {/* <Route exact path="/Gallery" element={<Login/>}/>
+          <Route exact path="/Pricing" element={<RecoveryPassword />} />
+          <Route exact path="/About" element={<RecoveryPassword/>}/>
+          <Route exact path="/Blog" element={<RecoveryPassword />} />
+          <Route exact path="/Contact" element={<RecoveryPassword/>}/> */}
+          {/* <Route path="*" element={<NotFound/>}/> */}
+        </Routes>
+    </Router>
   );
 }
 
