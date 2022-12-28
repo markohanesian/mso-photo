422b5a77e25d5dfb183186e433f6d81b5798567d
mso872 <mso872@gmail.com>
2022-12-27 22:11:37 -0800

    add NavBar to Home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from "../../components/nav/NavBar";
 import MasonryImageList from "../../components/imageList/ImageList"
 
-export default function home() {
+export default function Home() {
     return (
         <>
+            <NavBar />
             <MasonryImageList />
          </>
     );
