c9945fad1e6ce4323fe51c0a4a06f3c5fbba3999
mso872 <mso872@gmail.com>
2022-12-27 21:52:28 -0800

    Create basic home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MasonryImageList from "../../components/imageList/ImageList"
+
+export default function home() {
+    return (
+        <>
+            <MasonryImageList />
+         </>
+    );
+};
