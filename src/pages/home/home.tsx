6dcf128f508018dce2522590f6079c10895b3514
mso872 <mso872@gmail.com>
2023-06-30 14:35:35 -0700

    Add Hero to homepage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from "../../components/nav/NavBar";
 import MasonryImageList from "../../components/imageList/ImageList"
+import Hero from "../../components/hero/Hero";
 
 export default function Home() {
     return (
         <>
             <NavBar />
+            <Hero />
             <MasonryImageList />
          </>
     );
