38c29a0cfea3d4c90f47659243e0d650ab044215
mso872 <mso872@gmail.com>
2023-06-30 13:26:59 -0700

    Update services and contact

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -3,11 +3,9 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Container from "@mui/material/Container";
-
+import Typography from "@mui/material/Typography";
 import Navbar
     from '../../components/nav/NavBar';
 import MasonryImageList from '../../components/imageList/ImageList'
@@ -15,49 +13,45 @@ import MasonryImageList from '../../components/imageList/ImageList'
 
 function createData(
     title: string,
-    description: number,
-    price: number,
+    description: string,
+    price: string | number,
 ) {
     return { title, description, price };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Eclair', 262, 16),
-    createData('Cupcake', 305, 3.7),
-    createData('Gingerbread', 356, 16.0),
+    createData('Headshots', 'Shot in studio with professional lighting. Group sessions for entire company/team discounted depending on size', 'Starting at $250 for 3 photos for one individual ($75 each additional)'),
+    createData('Branding', 'Standard Small Business Package: 20 photos for marketing your brand delivered month to month so you always have new content ', 'starting at $800'),
+    createData('Products', 'Standard photos on white background for ecommerce/online, oversized items, Styled product photos (photos that show brand colors, product ingredients, use props, etc) - subject to hourly rate (message for details) ', 'starting at $75/photo'),
+    createData('Events', 'Parties, corporate, etc - can add second photographer or videographer for additional cost', 'Starting at $600 for 2 hour photoshoot'),
+    createData('Weddings', 'Creative and candid wedding photos that tell a stunning story of your dream day. Two photographers will be on location to capture multiple angles of all important moments', 'Starting at $5000 for full-day rate (8 hours)'),
 ];
 
 export default function Services() {
     return (
         <>
-
             <Navbar />
             <Container maxWidth="lg" sx={{
                 padding: "3rem"
             }}>
-
-                <TableContainer component={Paper}>
+                <Typography variant="h1" sx={{
+                    margin: "1rem 0 3rem 0"
+                }}>
+                    Services
+                </Typography>
+                <TableContainer /*component={Paper}*/>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Service</TableCell>
-                                <TableCell align="right">description</TableCell>
-                                <TableCell align="right">price</TableCell>
-                            </TableRow>
-                        </TableHead>
                         <TableBody>
                             {rows.map((row) => (
                                 <TableRow
                                     key={row.title}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
-                                    <TableCell component="th" scope="row">
+                                    <TableCell sx={{ verticalAlign: 'top' }} component="th" scope="row">
                                         {row.title}
                                     </TableCell>
-                                    <TableCell align="right">{row.description}</TableCell>
-                                    <TableCell align="right">{row.price}</TableCell>
+                                    <TableCell sx={{ verticalAlign: 'top' }} align="left">{row.description}</TableCell>
+                                    <TableCell sx={{ verticalAlign: 'top' }} align="left">{row.price}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
