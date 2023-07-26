1bde27acc6478df138fa78c1d61ff40e280cccb0
mso872 <mso872@gmail.com>
2023-07-26 12:27:22 -0700

    Move text out of About, spacing

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -18,24 +18,13 @@ export default function About() {
                     About
                 </Typography>
                 <Typography variant="body1" sx={{
-                    marginBottom: "1rem"
+                    marginBottom: "4rem"
                 }}>
                     Mark Ohanesian is a digital photographer that specializes in product, portrait, and branding photography,
                     with over a decade of experience. Mark uses professional
                     quality cameras, lenses, and lighting equipment, and offers professional retouching
                     and digital enhancement to in order to capture and enhance the highest quality
                     imagery.
-                </Typography>
-                <Typography variant="body1" gutterBottom sx={{
-                    marginBottom: "3rem"
-                }}>
-
-                    The popularity of online consumerism means that your personal
-                    brand/company image starts with an image. People want to see
-                    authentic, beautiful, and captivating photos of goods and services
-                    that tell a story and enhance your brand. Set your brand apart with
-                    beautiful imagery so you can SHOW the world how great it is without
-                    having to say a word.
                 </Typography>
                 <Typography variant="h2" sx={{
                     margin: "1rem 0 ", fontSize: "24px"
