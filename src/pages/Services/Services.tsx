d1663a5b67fb1b5f2cc9f1733c12f3ef6c98ded9
mso872 <mso872@gmail.com>
2024-02-10 08:52:11 -0800

    Add buttons to services

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -6,44 +6,66 @@ import MasonryImageList from "../../components/imageList/ImageList";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 
 function createServiceItem(
   title: string,
   description: string,
-  price: string | number
+  price: string | number,
+  link = { url: "", text: "" }
 ) {
-  return { title, description, price };
+  return { title, description, price, link };
 }
 
 const serviceItems = [
   createServiceItem(
     "Headshots",
     "Shot with professional lighting \nRetouched to naturally remove flaws  \nGroup discounts available  \n \nPricing is for 3 edited photos from the session",
-    "$250"
-  ),
-  createServiceItem(
-    "Branding",
-    "Standard Small Business Package: 20 photos for marketing your brand delivered month to month so you always have new content",
-    "$800"
-  ),
-  createServiceItem(
-    "Products",
-    "Standard photos on white background for ecommerce/online, oversized items, Styled product photos (photos that show brand colors, product ingredients, use props, etc) - subject to hourly rate (message for details)",
-    "$75/photo"
+    "$250",
+    { url: "https://calendly.com/mso872/headshot-session", text: "book" }
   ),
   createServiceItem(
     "Events",
     "Parties, corporate, etc - can add a second photographer or videographer for an additional cost  \n \npricing is for a 2-hour photoshoot",
-    "$600"
+    "$600",
+    { url: "https://calendly.com/mso872/event-session", text: "book" }
   ),
   createServiceItem(
-    "Weddings",
-    "Creative and candid wedding photos that tell a stunning story of your dream day. Two photographers will be on location to capture multiple angles of all important moments \n \n Pricing is for a full-day rate (8 hours)",
-    "$5000"
+    "Branding",
+    "Standard Small Business Package: 20 photos for marketing your brand delivered month to month so you always have new content",
+    "$800",
+    { url: "https://calendly.com/mso872/branding-consultation", text: "book" }
   ),
+  // createServiceItem(
+  //   "Products",
+  //   "Standard photos on white background for ecommerce/online, oversized items, Styled product photos (photos that show brand colors, product ingredients, use props, etc) - subject to hourly rate (message for details)",
+  //   "$75/photo"
+  // ),
+  // createServiceItem(
+  //   "Weddings",
+  //   "Creative and candid wedding photos that tell a stunning story of your dream day. Two photographers will be on location to capture multiple angles of all important moments \n \n Pricing is for a full-day rate (8 hours)",
+  //   "$5000"
+  // ),
 ];
 
+const linkStyle = {
+  display: 'flex',
+  justifyContent: "center", 
+  backgroundColor: "#fff",
+  color: "rgba(0, 0, 0, 0.5)",
+  textTransform: 'uppercase',
+  padding: ".5rem 1rem",
+  borderRadius: "4px",
+  textDecoration: "none",
+  transition: "background-color 0.3s ease",
+  "&:hover": { // Define hover styles
+    backgroundColor: "rgb(162, 170, 255)", // Darken background color on hover
+    color: "black", // Change text color to white on hover
+  },
+}
+
 export default function Services() {
+  
   return (
     <>
       <Navbar />
@@ -89,9 +111,12 @@ export default function Services() {
                 >
                   {service.title}
                 </Typography>
-                <Typography variant="body2" sx={{ whiteSpace: "pre-line", lineHeight: '1.5rem' }}>
+                <Typography variant="body2" sx={{ whiteSpace: "pre-line", lineHeight: '1.5rem', marginBottom: '1rem' }}>
                   {service.description}
                 </Typography>
+                {service.link && service.link.url && service.link.text ? (
+                <Link sx={linkStyle} href={service.link.url}>{service.link.text}</Link>
+              ) : null}
               </CardContent>
             </Card>
           ))}
